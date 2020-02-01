504f55e768c0a371d34fd7399efaa86f078ad18f
mateo <m.devia@uniandes.edu.co>
2020-02-01 11:57:37 -0500

    string bug fix

--- model/models.py
+++ model/models.py
@@ -11,7 +11,7 @@ class User(models.Model):
     email = models.CharField(max_length=1000)
 
     def __str__(self):
-        return 'User: ' + self.idUser
+        return 'User: ' + str(self.idUser)
 
 
 class Pais(models.Model):
@@ -19,7 +19,8 @@ class Pais(models.Model):
     nombre = models.CharField(max_length=200)
 
     def __str__(self):
-        return 'Pais: ' + self.idPais
+        return 'Pais: ' + str(self.idPais)
+
 
 class Delegacion(models.Model):
     idDelegacion = models.AutoField(primary_key=True)
@@ -27,7 +28,7 @@ class Delegacion(models.Model):
     habilitado = models.BooleanField()
 
     def __str__(self):
-        return 'Delegacion: ' + self.idDelegacion
+        return 'Delegacion: ' + str(self.idDelegacion)
 
 
 class Entrenador(models.Model):
@@ -36,15 +37,16 @@ class Entrenador(models.Model):
     apellido = models.CharField(max_length=50)
 
     def __str__(self):
-        return 'Entrenador: ' + self.idEntrenador
+        return 'Entrenador: ' + str(self.idEntrenador)
+
 
 class Deporte(models.Model):
     idDeporte = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=100)
 
-
     def __str__(self):
-        return 'Deporte: ' + self.idDeporte
+        return 'Deporte: ' + str(self.idDeporte)
+
 
 class Ciudad(models.Model):
     idCiudad = models.AutoField(primary_key=True)
@@ -52,7 +54,7 @@ class Ciudad(models.Model):
     nombre = models.CharField(max_length=100)
 
     def __str__(self):
-        return 'Ciudad: ' + self.idCiudad
+        return 'Ciudad: ' + str(self.idCiudad)
 
 
 class Deportista(models.Model):
@@ -63,9 +65,9 @@ class Deportista(models.Model):
     nombre = models.CharField(max_length=100)
     apellido = models.CharField(max_length=100)
     edad = models.IntegerField()
-    peso = models.DecimalField(max_digits=6,decimal_places=2)
-    estatura = models.DecimalField(max_digits=6,decimal_places=2)
+    peso = models.DecimalField(max_digits=6, decimal_places=2)
+    estatura = models.DecimalField(max_digits=6, decimal_places=2)
     foto = models.CharField(max_length=250)
 
     def __str__(self):
-        return 'Deportista: ' + self.idDeportista
+        return 'Deportista: ' + str(self.idDeportista)
