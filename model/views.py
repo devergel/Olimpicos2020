e4bde1f9a52f07223d281e8a4d318c1db8702cc1
dvergel <dvergel@amantium.net>
2020-02-09 00:34:25 -0500

    Adicion de back-end registrar Usuario django

--- model/views.py
+++ model/views.py
@@ -1,4 +1,11 @@
+import json
+
+from django.contrib.auth.models import User
+from django.core import serializers
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+
 from .models import Deportista
 from django.core.paginator import Paginator
 # Create your views here.
@@ -25,3 +32,22 @@ def deportes_list(request):
     }
     print(context)
     return render(request, 'deportistas/deportistas_list.html', context)
+
+
+@csrf_exempt
+def add_user_view(request):
+    if request.method == 'POST':
+        jsonUser = json.loads(request.body)
+        username = jsonUser['username']
+        first_name = jsonUser['first_name']
+        last_name = jsonUser['last_name']
+        password = jsonUser['password']
+        email = jsonUser['email']
+
+        user_model = User.objects.create_user(username=username, password=password)
+        user_model.first_name = first_name
+        user_model.last_name = last_name
+        user_model.email = email
+        user_model.save()
+    return HttpResponse(serializers.serialize("json", [user_model]))
+
--- modelProject/urls.py
+++ modelProject/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
 from model import views
@@ -25,4 +26,5 @@ urlpatterns = [
     path('', views.deportes_list),
     path('deportistas/', views.deportes_list),
 
+    url('addUser/$', views.add_user_view, name='addUser'),
 ]
